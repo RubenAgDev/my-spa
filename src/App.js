7ae35036b0bc409eeddcbdde23ab29cdcb7a4d44
Ruben Aguilar <ruben.ag@Rubens-MBP.hsd1.ca.comcast.net>
2019-09-22 22:37:50 -0700

    Integration with Space Explorer Micro Frontend

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am running from:
-        </p>
-        <p>
-          <code>{RUNNING_FROM}</code>
+          I am running from: <code>{RUNNING_FROM}</code>
         </p>
       </header>
     </div>
